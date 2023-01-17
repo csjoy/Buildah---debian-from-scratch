295f67d3a19839588cb917b3e1f5c4f41d7928c1
csjoy <antohin.joy@gmail.com>
2023-01-17 13:04:30 +0600

    init

---
+++ debian-from-scratch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Defining default image name
+IMAGE_NAME="${1:-bookworm}"
+
+echo "### Checking for prerequisite"
+
+# Check if the user has privileged access
+if [ $UID -ne 0 ]; then
+  echo "Run buildscript as root user"
+  exit 1
+fi
+
+# Check if buildah is present in host os
+if [ -z "$(command -v buildah)" ]; then
+  echo "buildah is not installed"
+  exit 1
+fi
+
+# Check if debootstrap is present in host os
+if [ -z "$(command -v debootstrap)" ]; then
+  echo "debootstrap is not installed"
+  exit 1
+fi
+
+echo "### Testing container creation"
+buildah from scratch
+if [ $? -ne 0 ]; then
+  echo "Error initializing working container"
+  exit 1
+fi
+
+echo "### Mounting working container"
+scratchmount=$(buildah mount working-container)
+if [ $? -ne 0 ]; then
+  echo "Error mounting working container"
+  exit 1
+fi
+
+echo "### Bootstraping debian base image"
+debootstrap --variant=minbase bookworm $scratchmount
+if [ $? -ne 0 ]; then
+  echo "Error creating minbase image"
+  exit 1
+fi
+
+echo "### Commiting final image"
+buildah commit working-container $IMAGE_NAME
+if [ $? -ne 0 ]; then
+  echo "Error commiting image"
+  exit 1
+fi
+
+echo "### Build debian from scratch completed successfully!"
+exit 0
